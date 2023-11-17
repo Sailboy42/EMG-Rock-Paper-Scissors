7d0a60d200ec70617eff9a3fa9d8bba9359eb2aa
acodringtonolin <91296649+acodringtonolin@users.noreply.github.com>
2023-11-17 03:18:43 -0500

    add online classification code

---
+++ OnlineClassification/ChosenFeatures.py
@@ -0,0 +1,64 @@
+import numpy as np
+from statsmodels.tsa.ar_model import AutoReg
+
+# Assuming the data shape is: channels x timepoints x trials
+# The following feature functions are designed to process this data
+
+def variance(data):
+    # Variance calculated over timepoints for each channel
+    return np.var(data, axis=1).T  # Transpose to make it trials x channels
+
+def mean_absolute_value(data):
+    # Mean Absolute Value calculated over timepoints for each channel
+    return np.mean(np.abs(data), axis=1).T
+
+def root_mean_square(data):
+    # Root Mean Square calculated over timepoints for each channel
+    return np.sqrt(np.mean(np.square(data), axis=1)).T
+
+def zero_crossings(data):
+    # Zero Crossing calculated over timepoints for each channel
+    zc = np.sum(np.abs(np.diff(np.sign(data), axis=1)) == 2, axis=1)
+    return zc.T
+
+def waveform_length(data):
+    # Waveform Length calculated over timepoints for each channel
+    wl = np.sum(np.abs(np.diff(data, axis=1)), axis=1)
+    return wl.T
+
+def mean_frequency(data):
+    # Mean Frequency calculated over timepoints for each channel
+    sp = np.fft.fft(data, axis=1)
+    freq = np.fft.fftfreq(data.shape[1])
+    p = np.abs(sp)**2
+    mean_freq = np.sum(p * freq.reshape((1, -1)), axis=1) / np.sum(p, axis=1)
+    return mean_freq.T
+
+def autoregressive_coefficients(data):
+    # Autoregressive Coefficients calculated over timepoints for each channel
+    num_channels, num_trials = data.shape
+    ar_coeffs = np.zeros((num_channels))
+
+    for i in range(num_channels):
+    # for j in range(num_trials):
+        model = AutoReg(data[i, :], lags=1)
+        model_fitted = model.fit()
+        ar_coeffs[i] = model_fitted.params[1]
+
+    return ar_coeffs
+
+def extract_features(data):
+    # Extract and combine all features
+    var = variance(data)
+    mav = mean_absolute_value(data)
+    rms = root_mean_square(data)
+    zc = zero_crossings(data)
+    wl = waveform_length(data)
+    mf = mean_frequency(data)
+    ar = autoregressive_coefficients(data)
+
+    # Concatenate all features horizontally
+    combined_features = np.hstack((var, mav, rms, zc, wl, mf, ar))
+
+
+    return combined_features
---
+++ OnlineClassification/PreProcess.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy.signal import butter, filtfilt
+
+def highpass(hpdata, cutoff = 20, hpfs = 1000):
+    b, a = butter(1, cutoff, "hp", fs=hpfs, analog=False, output="ba")
+    return filtfilt(b, a, hpdata, axis=0, padlen=2)
+
+
+def preprocess_data(lsl_data):
+    # Set parameters
+    Fs = 1000
+    num_ch = 4
+    
+    #labels = []
+
+    # Check sampling frequency
+    actual_fs = 1 / np.mean(np.diff(lsl_data[:, 0]))
+    #if np.abs(actual_fs - Fs) > 50:
+        #warning("Actual Fs and Fs are quite different. Please check sampling frequency.")
+
+    # Filter data
+    filtered_lsl_data = np.zeros_like(lsl_data)
+    filtered_lsl_data[:, 0] = lsl_data[:, 0]
+    for ch in range(num_ch):
+        filtered_lsl_data[:, 1+ch] = highpass(lsl_data[:, ch+1], 5, Fs)
+
+
+    return filtered_lsl_data #returns time stamps and high pass filtered data for each channel
+    # format of time stamps, rock, paper, scissors
