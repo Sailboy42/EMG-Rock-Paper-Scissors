0ca34a9eafc659ded5a5c8318837b45c924f6d48
acodringtonolin <91296649+acodringtonolin@users.noreply.github.com>
2023-12-01 01:41:04 -0500

    update file
    updating file

--- NewOnlineClassification/TestOnlineClassification.py
+++ NewOnlineClassification/TestOnlineClassification.py
@@ -10,18 +10,22 @@ def english_label(label):
     else:
         return "scissors"
 
-test_mat = scipy.io.loadmat("OfflineAnalysis/exampleEMGdata120trial_test.mat")
+test_mat = scipy.io.loadmat("DataFile/allSubjFiles.mat")
+#this is supposed to be isl data
+#test data should be filtered data from the preprocessing, datachtimetr
+#test_mat1 = preprocesspt2()
 test_data = test_mat["dataChTimeTr"]
 test_labels = test_mat["labels"].flatten()
 
 # (1400,4) trial
+#choosing the first entry in test data
 test_trial_raw = test_data[:, :, 0].T
 
 test_trial = np.zeros((4, test_trial_raw.shape[0]))
 test_trial = test_trial_raw.T
 test_trial_label = test_labels[0]
 
-model_filename = "/Users/yumi/Documents/Neurotech/Owen Aaron Project/Online_Classification/SamData_knn_model.pkl"
+model_filename = "/Users/yumi/Documents/Neurotech/Owen Aaron Project/Online_Classification/TrainedKNNMODEL.pkl"
 
 rpm = RunPythonModel(model_filename)
 result = rpm.get_rps(test_trial)
